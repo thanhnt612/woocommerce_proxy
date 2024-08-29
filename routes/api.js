00b0a4f5f56a1f38abdadc19a15cd7a3e8fc3e9e
thanhnt612 <nguyentruongthanh0612@gmail.com>
2024-08-29 08:55:47 +0700

    update proxy server test

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const proxyController = require('../controllers/proxyController');
-
-const router = express.Router();
-
-router.use('/api/*', proxyController.handleProxyRequest);
-
-module.exports = router;
